fbbe7ea4cadebb8881e78503ab1e37eb41e47851
アンNè <73763455+antlt1@users.noreply.github.com>
2022-09-18 19:53:58 +0700

    update js index

--- source/js/index.js
+++ source/js/index.js
@@ -3,10 +3,8 @@ var nav = document.getElementsByClassName('nav')[0];
 // console.log(nav);
 window.onscroll = function (e) { 
     var scroll = window.pageYOffset;
-    console.log(scroll);
     if(scroll >= 100){
         nav.style.backgroundColor = 'white';
-        console.log('trong: '+scroll);
     }else if(scroll == 0){
         nav.style.backgroundColor = 'transparent';
     }
